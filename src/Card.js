825146aeff95e29701603a5e8337e83f4034e686
Sven Hoevink <svenhoevink@gmail.com>
2020-10-07 23:48:19 +0200

    added Card component with basic info and picture

---
+++ src/Card.js
@@ -0,0 +1,18 @@
+import React, { Fragment } from 'react';
+import 'tachyons';
+
+const Card = ({ name, id, email }) => {
+	return (
+		<Fragment>
+			<div className="bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5 tc">
+				<img alt="photo" src={`https://robohash.org/${id}?size=200x200`} />
+				<div>
+					<h2>{name}</h2>
+					<p>{email}</p>
+				</div>
+			</div>
+		</Fragment>
+	);
+};
+
+export default Card;
